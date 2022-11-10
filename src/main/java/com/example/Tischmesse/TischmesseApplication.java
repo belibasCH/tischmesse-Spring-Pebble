3850a9f15cbb0ab1afd3d466521e54f2971cee33
unknown <elias.braem@students.fhnw.ch>
2022-11-10 14:36:06 +0100

    add spring intializer project

---
+++ src/main/java/com/example/Tischmesse/TischmesseApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tischmesse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TischmesseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TischmesseApplication.class, args);
+	}
+
+}
