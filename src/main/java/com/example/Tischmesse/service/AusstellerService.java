912528567fd4d38597193f6ad023d1da43577f4a
unknown <elias.braem@students.fhnw.ch>
2022-11-10 17:35:27 +0100

    add aussteller Service

---
+++ src/main/java/com/example/Tischmesse/service/AusstellerService.java
@@ -0,0 +1,13 @@
+package com.example.Tischmesse.service;
+
+import com.example.Tischmesse.model.Aussteller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AusstellerService {
+    private List<Aussteller> ausstellerListe = new ArrayList<Aussteller>();
+
+
+
+}
