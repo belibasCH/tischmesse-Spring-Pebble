0fe9445488c8806708dec0b043fb055ae074e31c
unknown <elias.braem@students.fhnw.ch>
2022-11-10 17:11:46 +0100

    add controllers

--- src/main/java/com/example/Tischmesse/controller/TischmesseController.java
+++ src/main/java/com/example/Tischmesse/controller/TischmesseController.java
@@ -2,12 +2,32 @@ package com.example.Tischmesse.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TischmesseController {
 
     @GetMapping("/admin")
-    public String admin() {
+    public String getAdminOverwiew() {
         return "/admin";
+    }
+
+    @GetMapping("/aussteller")
+    public String showAussteller() {
+        return "/aussteller";
+    }
+
+    @GetMapping("/anmeldung")
+    public String anmeldung() {
+        return "/anmeldung";
+    }
+    @PostMapping("/anmeldung")
+    public String addAussteller(@RequestParam String firstName,
+                             @RequestParam  String lastName,
+                             @RequestParam String jobTitle,
+                             @RequestParam String company) {
+        //var created =
+        return "redirect:/aussteller/" + created.getId(); ;
     }
 }
