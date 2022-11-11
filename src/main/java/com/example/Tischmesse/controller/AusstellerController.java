2e37ea503ce67ad24ba0c40669d6afe5b4c5bf77
unknown <elias.braem@students.fhnw.ch>
2022-11-11 16:36:26 +0100

    erster Aussteller in der admin ansicht geschafft!

--- src/main/java/com/example/Tischmesse/controller/AusstellerController.java
+++ src/main/java/com/example/Tischmesse/controller/AusstellerController.java
@@ -1,17 +1,23 @@
 package com.example.Tischmesse.controller;
 
+import com.example.Tischmesse.service.AusstellerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AusstellerController {
 
+    private final AusstellerService service;
 
+    public AusstellerController(AusstellerService service) {
+        this.service = service;
+    }
     @GetMapping("/aussteller")
-    public String showAussteller() {
+    public String showAussteller(Model model) {
+        model.addAttribute("aussterllerListe", service.getAusstellerListe());
         return "/aussteller";
     }
-
     @GetMapping("/anmeldung")
     public String anmeldung() {
         return "/anmeldung";
@@ -22,6 +28,7 @@ public class AusstellerController {
 //                             @RequestParam String jobTitle,
 //                             @RequestParam String company) {
 //        //var created =
+
 //        return "redirect:/aussteller/" + created.getId();
 //    }
 
