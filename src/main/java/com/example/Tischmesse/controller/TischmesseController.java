9f0880eb00a19de0b51296cdc61ebfc2fe3e73e7
Joel Finger <joel.finger@students.fhnw.ch>
2022-11-14 08:31:35 +0100

    created branch

--- src/main/java/com/example/Tischmesse/controller/TischmesseController.java
+++ src/main/java/com/example/Tischmesse/controller/TischmesseController.java
@@ -2,12 +2,19 @@ package com.example.Tischmesse.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TischmesseController {
 
     @GetMapping("/admin")
-    public String admin() {
+    public String getAdminOverwiew() {
         return "/admin";
     }
+
+
+    @GetMapping("/")
+    public String home(){ return "/home";}
+
 }
