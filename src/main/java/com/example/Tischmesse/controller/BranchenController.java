f540ad6b8bca53d6739af7efb2335f261b3727d7
Joel Finger <joel.finger@students.fhnw.ch>
2022-11-18 09:40:57 +0100

    Branchen-Editor hinzugefügt

--- src/main/java/com/example/Tischmesse/controller/BranchenController.java
+++ src/main/java/com/example/Tischmesse/controller/BranchenController.java
@@ -1,12 +1,15 @@
 package com.example.Tischmesse.controller;
 
 import com.example.Tischmesse.service.BranchenService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class BranchenController {
@@ -24,11 +27,30 @@ public class BranchenController {
 
     @PostMapping("/branchen")
     public String showBranchen(@RequestParam String branchenname, Model model){
+        //checkBranchenname(branchenname);
         model.addAttribute("branchenListe", branchenService.addBranche(branchenname));
         model.addAttribute("branchenListe", branchenService.getBranchenListe());
         return "/branchen";
     }
 
+    /*
+
+    private void checkBranchenname(String branchenName){
+        if(branchenName == null || branchenName.length() < 4){
+            throw new InvalidBranchenName();
+        }
+    }
+
+    @ExceptionHandler(InvalidBranchenName.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String invalidBranchenName(Model model){
+        model.addAttribute("errorMessage", "Dein Branchenname ist leer.");
+        return "/branchen";
+    }
+
+    private static class InvalidBranchenName extends RuntimeException{}
+
+     */
 
 
 }
